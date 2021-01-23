b917d684d675d0fabc57e3c8aa80e0d31007a97d
DenizardDimitri-Camargo <62820018+DenizardDimitri-Camargo@users.noreply.github.com>
2021-01-23 00:47:28 -0400

    MySQL adaptation and first migration
    Diz: MySQL adaptation mas na verdade eu uso SQL Server

--- SalesWebMvcUpdate/Startup.cs
+++ SalesWebMvcUpdate/Startup.cs
@@ -37,7 +37,8 @@ namespace SalesWebMvcUpdate
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebMvcUpdateContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcUpdateContext")));
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcUpdateContext"), builder =>
+                        builder.MigrationsAssembly("SalesWebMvcUpdate")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
